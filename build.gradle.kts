8b8117c070af8436487869f26a826167c21f61a5
Martin Bonnin <martin@mbonnin.net>
2022-06-07 17:46:58 +0200

    publish to the Gradle plugin portal

--- build.gradle.kts
+++ build.gradle.kts
@@ -3,7 +3,7 @@ plugins {
     id("java-gradle-plugin")
     id("com.gradleup.gr8.external").version("0.5") // only for removeGradleApiFromApi
     id("org.jetbrains.kotlin.plugin.sam.with.receiver")
-    id("net.mbonnin.sjmp")
+    id("com.gradle.plugin-publish").version("1.0.0-rc-2")
 }
 
 repositories {
@@ -33,17 +33,28 @@ gradlePlugin {
     }
 }
 
+pluginBundle {
+    // These settings are set for the whole plugin bundle
+    website = "https://github.com/martinbonnin/sjmp"
+    vcsUrl = "https://github.com/martinbonnin/sjmp"
+
+    // tags and description can be set for the whole bundle here, but can also
+    // be set / overridden in the config for specific plugins
+    description = "Greetings from here!"
+    tags = listOf("sjmp", "publish")
+}
+
 samWithReceiver {
     annotation("org.gradle.api.HasImplicitReceiver")
 }
 
-sjmp {
-    configureAllPublications {
-        pomGithubRepository = "martinbonnin/sjmp"
-        pomLicense = "MIT License"
-        pomGithubLicensePath = "blob/main/LICENSE"
-    }
-}
+//sjmp {
+//    configureAllPublications {
+//        pomGithubRepository = "martinbonnin/sjmp"
+//        pomLicense = "MIT License"
+//        pomGithubLicensePath = "blob/main/LICENSE"
+//    }
+//}
 
 tasks.withType(JavaCompile::class.java).configureEach {
     options.release.set(8)
